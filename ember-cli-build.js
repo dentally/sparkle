a14e7dcc6c2900cc0f8fef0592293fd595e03209
Kyle Nathan <kyle@wearelion.com>
2025-02-03 17:02:51 +0000

    Adds ember-concurrency v4 arrow function support

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,11 @@ module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
 
-    // Add options here
+    babel: {
+      plugins: [
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+      ],
+    },
   });
 
   const { Webpack } = require('@embroider/webpack');
